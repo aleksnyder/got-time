743fee5cdf26c51817a09bdc1226f56d04049118
Alek Snyder <asnyder0816@gmail.com>
2023-12-26 11:24:10 -0500

    ci: use repo name for gh pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: 'https://aleksnyder.github.io/got-time/',
+  base: '/got-time/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
