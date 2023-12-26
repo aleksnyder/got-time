06b25bba71c0b7a6dd69bcd663d9ffc2672920bd
Alek Snyder <asnyder0816@gmail.com>
2023-12-26 11:19:07 -0500

    ci: add base url for gh pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  base: 'https://aleksnyder.github.io/got-time/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
